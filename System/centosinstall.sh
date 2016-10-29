203ac0e6d3be27193b51ad251b6e914c1e9ecb98
AlexWoo(武杰) <wj19840501@gmail.com>
2016-10-29 15:11:28 +0800

    add debug and develop install

--- System/centosinstall.sh
+++ System/centosinstall.sh
@@ -1,8 +1,12 @@
-#!/usr/bash
+#!/bin/bash
 
 # yum repos
 curl -L  http://mirrors.163.com/.help/CentOS7-Base-163.repo -o /etc/yum.repos.d/CentOS-163.repo
 curl -L  http://mirrors.aliyun.com/repo/Centos-7.repo -o /etc/yum.repos.d/CentOS-aliyun.repo
+if [ ! -f /etc/yum.repos.d/CentOS-Base.repo.orig ];then
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.orig
+fi
+cp /etc/yum.repos.d/CentOS-aliyun.repo /etc/yum.repos.d/CentOS-Base.repo
 
 # for network
 yum install -y net-tools
@@ -20,9 +24,32 @@ yum install -y svn
 yum install -y man-pages
 yum install -y dos2unix
 
+# for debug tools
+yum install -y dstat
+yum install -y strace
+yum install -y gperf
+yum install -y perf
+yum install -y systemtap
+yum install -y systemtap-sdt-devel #drace
+# flame graph
+git clone https://github.com/brendangregg/FlameGraph.git  /usr/local/flamegraph
+cp flameperf.sh /usr/local/bin/
+
 # for gcc and g++
 yum install -y gcc
 yum install -y gcc-c++
+yum install -y gdb
+# cgdb
+yum install -y ncurses-devel flex texinfo help2man readline-devel
+wget https://cgdb.me/files/cgdb-0.6.8.tar.gz
+tar xzf cgdb-0.6.8.tar.gz
+cd cgdb-0.6.8
+./configure --prefix=/usr/local
+make && make install
+cd ..
+
+# for python
+yum install -y python-devel
 
 # for vim
 yum install -y vim
